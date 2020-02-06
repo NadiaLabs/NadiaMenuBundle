8f82b49a6837261c7eba5c6741a14fe6466cc207
Leo <leo.on.the.earth@gmail.com>
2020-02-06 19:19:46 +0800

    Fix default cache adapter service id

--- tests/Fixtures/config/test-cache.php
+++ tests/Fixtures/config/test-cache.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'adapter' => 'cache.adapter.array',
+    'adapter' => 'cache.app',
     'ttl' => 604800,
     'group_key' => '_nadia_menu_cache_group',
     'check_item_changes' => true,
