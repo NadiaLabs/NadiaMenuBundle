5f5d90c148d3ec8c40e4246c3dd77d806acb0dff
Leo <leo.on.the.earth@gmail.com>
2022-03-05 19:09:26 +0800

    Remove old files

--- tests/Fixtures/config/test-cache.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'adapter' => 'cache.app',
-    'ttl' => 604800,
-    'group_key' => '_nadia_menu_cache_group',
-    'check_item_changes' => true,
-];
--- tests/Fixtures/config/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$container->loadFromExtension('nadia_menu', [
-    'cache' => require __DIR__ . '/test-cache.php',
-    'menus' => require __DIR__ . '/test-menus.php',
-    'menu_providers' => require __DIR__ . '/test-menus-providers.php',
-    'knp_menus' => ['test', 'test2'],
-]);
